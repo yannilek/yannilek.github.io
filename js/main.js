173451d7a306cda2bf92fedb6ca20414701c3889
yannilek <yann@ilek.fr>
2023-02-19 19:18:02 +0100

    add text

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 const cards = [
   {
     image: './images/temperance.png',
-    title: 'Temperance',
+    title: 'Nos activités sociales',
     color: 'violet',
-    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+    text: 'Comprendre les besoins de chacun.<br>Définir des <span>avantages</span> accessibles <span>pour tous</span> à Toulouse et ailleurs.',
+    cfdText: 'Avec la CFDT préparez <span>l\'avenir</span>.',
   },
   {
     image: './images/papesse.png',
-    title: 'Papesse',
+    title: 'Nos rémunérations',
     color: 'volt',
-    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+    text: 'Définir des règles <span>justes et factuelles</span> pour nos rémunérations, nos évolutions professionnelles, la reconnaissance de notre travail.',
+    cfdText: 'Avec la CFDT soyez <span>accompagné</span>.',
   },
   {
     image: './images/justice.png',
-    title: 'Justice',
+    title: 'Nos conditions de travail',
     color: 'violet',
-    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+    text: 'Mettre en place et développer des actions concrètes et récurrentes pour <span>préserver notre santé</span>.<br>Combattre l\'isolement et <span>assurer la prévention</span> des risques.',
+    cfdText: 'Avec la CFDT préservez <span>votre santé</span>.',
   },
   {
     image: './images/monde.png',
-    title: 'Monde',
+    title: 'Notre temps de travail',
     color: 'volt',
-    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+    text: 'Négocier l\'évolution du temps de travail.<br>Préserver <span>l\'harmonie</span> entre <span>vie professionnelle</span> et <span>vie personnelle</span> pour tous.',
+    cfdText: 'Avec la CFDT soyez <span>représenté</span>.',
   },
 ];
 
@@ -35,18 +39,21 @@ for (let i = 0; i < cards.length; i++) {
         <div class="cardFace cardFaceFront">
           <img class="fullWidth" src=${cardData.image} />
         </div>
-        <div class="cardFace cardFaceBack d-flex">
+        <div class="cardFace cardFaceBack d-flex justify-content-center">
           <div class="cardContent d-flex flex-column">
-            <div class="cardHeader">
+            <div class="cardHeader d-flex justify-content-center">
               <img src=${cardData.image} alt="" class="pp" />
             </div>
-            <div class="cardBody text-center flex-grow-1">
-              <h3>${cardData.title}</h3>
-              <p>${cardData.text}</p>
+            <div class="cardBodyTitle text-center">
+              <p>${cardData.title}</p>
             </div>
-            <div class="pb-3 flex-shrink-1 d-flex justify-content-evenly">
-                <img class="w-25" src=${cardData.color === 'violet' ? './images/ilek-violet.svg' : './images/ilek-volt.svg' } />
-                <img class="w-25" src="./images/cfdt.png" />
+            <div class="cardBody text-center">${cardData.text}</div>
+            <div class="cardTagline text-center">
+              <p>${cardData.cfdText}</p>
+            </div>
+            <div class="cardFooter pb-3 d-flex justify-content-evenly">
+                <img class="footerLogo" src=${cardData.color === 'violet' ? './images/ilek-violet.svg' : './images/ilek-volt.svg' } />
+                <img class="footerLogo" src="./images/cfdt.png" />
             </div>
           </div>
         </div>
