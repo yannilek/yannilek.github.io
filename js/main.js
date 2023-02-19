35a7c1181aea1bb916acd8695164951ca5d929a0
yannilek <yann@ilek.fr>
2023-02-19 13:34:31 +0100

    ready to replace text

--- js/main.js
+++ js/main.js
@@ -1,21 +1,65 @@
-const cards = document.querySelectorAll(".cardInner");
+const cards = [
+  {
+    image: './images/temperance.png',
+    title: 'Temperance',
+    color: 'violet',
+    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+  },
+  {
+    image: './images/papesse.png',
+    title: 'Papesse',
+    color: 'volt',
+    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+  },
+  {
+    image: './images/justice.png',
+    title: 'Justice',
+    color: 'violet',
+    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+  },
+  {
+    image: './images/monde.png',
+    title: 'Monde',
+    color: 'volt',
+    text: 'lorem ipsum blablabla <span>fqsldkjf</span> qsldkfjq qslkdjf qslkdjf qsdkljf',
+  },
+];
 
-// const unflipAll = () => cards.forEach(card => card.classList.remove('isFlipped'));
+for (let i = 0; i < cards.length; i++) {
+  const cardData = cards[i];
+  const card = document.createElement('div');
+  card.className = `col-xl-6 ${cardData.color}`;
+  card.innerHTML = `
+    <div class="cardContainer">
+      <div class="cardInner">
+        <div class="cardFace cardFaceFront">
+          <img class="fullWidth" src=${cardData.image} />
+        </div>
+        <div class="cardFace cardFaceBack d-flex">
+          <div class="cardContent d-flex flex-column">
+            <div class="cardHeader">
+              <img src=${cardData.image} alt="" class="pp" />
+            </div>
+            <div class="cardBody text-center flex-grow-1">
+              <h3>${cardData.title}</h3>
+              <p>${cardData.text}</p>
+            </div>
+            <div class="pb-3 flex-shrink-1 d-flex justify-content-evenly">
+                <img class="w-25" src=${cardData.color === 'violet' ? './images/ilek-violet.svg' : './images/ilek-volt.svg' } />
+                <img class="w-25" src="./images/cfdt.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
 
-// cards.forEach(card => card.addEventListener("click", function (e) {
-//   let isFlipped = false;
-//   if (card.classList.contains('isFlipped')) {
-//     isFlipped = true;
-//   }
-//   unflipAll();
+  card.addEventListener("click", function (e) {
+    card.querySelector('.cardInner').classList.toggle('isFlipped');
+  });
 
-//   if (isFlipped) {
-//     card.classList.remove('isFlipped');
-//   } else {
-//     card.classList.add('isFlipped');
-//   }
-// }));
-
-cards.forEach(card => card.addEventListener("click", function (e) {
-  card.classList.toggle('isFlipped');
-}));
+  if (i < 2) {
+    document.querySelector('#cardsContainer1').append(card);
+  } else {
+    document.querySelector('#cardsContainer2').append(card);
+  }
+}
